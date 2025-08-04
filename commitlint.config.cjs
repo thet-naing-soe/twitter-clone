7a2f6dbfe614371da073cd7c3059afb60eb277b9
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-08-04 18:48:21 +0630

    chore: set up Husky, Prettier, lint-staged and Commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('@commitlint/types').UserConfig} */
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "perf",
+        "build",
+        "ci",
+        "revert",
+      ],
+    ],
+  },
+};
+
