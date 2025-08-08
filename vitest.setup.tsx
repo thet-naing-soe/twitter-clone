66539ce5369cab9c8b74f85e3212a597c6252d1c
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-08-08 11:09:49 +0630

    test(vitest): update vitest config and setup

--- vitest.setup.tsx
+++ vitest.setup.tsx
@@ -1,7 +1,9 @@
 import '@testing-library/jest-dom';
+import type { UrlObject } from 'url';
 import { vi } from 'vitest';
 
 import React from 'react';
+import type { AnchorHTMLAttributes, ImgHTMLAttributes, PropsWithChildren } from 'react';
 
 vi.mock('next/navigation', () => ({
   useRouter: () => ({
@@ -15,22 +17,54 @@ vi.mock('next/navigation', () => ({
   useSearchParams: () => new URLSearchParams(),
 }));
 
-vi.mock('next/image', () => ({
-  __esModule: true,
-  default: (props: any) => {
+// Types for the mocked <Image />
+type NextImageMockProps = ImgHTMLAttributes<HTMLImageElement> & {
+  alt?: string;
+};
+
+vi.mock('next/image', () => {
+  const MockedImage = (props: NextImageMockProps) => {
     // eslint-disable-next-line @next/next/no-img-element
     return <img {...props} alt={props.alt ?? ''} />;
-  },
-}));
+  };
 
-vi.mock('next/link', () => ({
-  __esModule: true,
-  default: ({ href, children, ...rest }: any) => (
-    <a href={typeof href === 'string' ? href : ''} {...rest}>
-      {children}
-    </a>
-  ),
-}));
+  return {
+    __esModule: true,
+    default: MockedImage,
+  };
+});
+
+// Types for the mocked <Link />
+type LinkHref = string | URL | UrlObject;
+type LinkMockProps = PropsWithChildren<
+  Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> & { href: LinkHref }
+>;
+
+vi.mock('next/link', () => {
+  const Link = ({ href, children, ...rest }: LinkMockProps) => {
+    let hrefStr = '';
+
+    if (typeof href === 'string') {
+      hrefStr = href;
+    } else if (href instanceof URL) {
+      hrefStr = href.toString();
+    } else {
+      const { pathname = '', hash = '', search } = href as UrlObject & { search?: string };
+      hrefStr = `${pathname}${search ?? ''}${hash ?? ''}`;
+    }
+
+    return (
+      <a href={hrefStr} {...rest}>
+        {children}
+      </a>
+    );
+  };
+
+  return {
+    __esModule: true,
+    default: Link,
+  };
+});
 
 vi.mock('next/font/google', () => ({
   Inter: () => ({ className: 'font-inter' }),
