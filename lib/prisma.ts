932e1d6e850f4adfe5616daaaefc841798beb58a
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-08-10 10:25:50 +0630

    fix: @typescript-eslint error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma: PrismaClient =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
